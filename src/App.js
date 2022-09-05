d18f0404b480b74ab4aab785f908d33124a5c340
Matiasmammela <mammela.matias@gmail.com>
2022-09-05 12:42:50 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, {useState} from 'react';
 import Search from './Search';
 import Pokemonit from './pokemonit';
-console.error('Täällä mitään vikailmoituksia ole :D');
-console.log = console.warn = console.error = () => {};
+//console.error('Täällä mitään vikailmoituksia ole :D');
+//console.log = console.warn = console.error = () => {};
 
 function App() {
   const [pokemoni,Setpokemonit]= useState(null);
--- src/Search.js
+++ src/Search.js
@@ -1,35 +1,48 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export default function Search(props) {
-    const [haku,setHaku]=useState("");
-    
-    function handleSubmit(e){
-        if(!haku){
-          props.Setpokemonit("Noresults");
-        }else{
-        let hakuLower=haku.toLowerCase();
-        const url='https://pokeapi.co/api/v2/pokemon/'+hakuLower;
-        axios.get(url)
-        .then(response =>{
-            props.Setpokemonit(response.data);
+  const [haku, setHaku] = useState("");
+  const [value, setValue] = useState("");
 
-              props.Setpokemonitcolor(response.data.types[0].type.name);
-          })
-        .catch((error) => { 
+  function handleSubmit(e) {
+    if (!haku) {
+      props.Setpokemonit("Noresults");
+    } else {
+      let hakuLower = haku.toLowerCase();
+      const url = 'https://pokeapi.co/api/v2/pokemon?limit=10000&offset=0';
+      axios.get(url).then(response => {
+        let resultsLike = response.data.results;
+        resultsLike.filter((val) => {
+          if (val.name.includes(hakuLower)) {
+            setValue(val.name);
+            return null;
+          }
+        })
+      })
+        .catch((error) => {
           props.Setpokemonit("Noresults");
         });
-        }
-        e.preventDefault();
     }
-    
+    e.preventDefault();
+  }
+  if (value) {
+    const url2 = 'https://pokeapi.co/api/v2/pokemon/' + value;
+    axios.get(url2).then(response2 => {
+      props.Setpokemonit(response2.data);
+
+      props.Setpokemonitcolor(response2.data.types[0].type.name);
+
+    })
+
+  }
   return (
     <>
       <div className="form-wrap">
-      <form onSubmit={handleSubmit}>
-          <input  autoComplete="off" placeholder="Pokemon's name" id='haku' type="text" value={haku} onChange={(e) => setHaku(e.target.value)}/>
-        <input id='nappi' type="submit" value="Search For a Pokemon!"/>
-      </form>
+        <form onSubmit={handleSubmit}>
+          <input autoComplete="off" placeholder="Pokemon's name" id='haku' type="text" value={haku} onChange={(e) => setHaku(e.target.value)} />
+          <input id='nappi' type="submit" value="Search For a Pokemon!" />
+        </form>
       </div>
     </>
   )
